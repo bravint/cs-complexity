d91744cce4e8dae08bb68cdb66939dd24054d703
bravint <bravint@gmail.com>
2022-03-21 18:12:13 +0000

    algorithms complete

--- src/bubbleSort.mjs
+++ src/bubbleSort.mjs
@@ -1,11 +1,20 @@
 /*
-Conceptual description:
-Time Complexity:
-Space Complexity:
+Conceptual description: loops over list sorting adjacent elements and swapping if unordered, inplace, stable
+Time Complexity: O(n²)
+Space Complexity: O(1) – constant space as sorting is done in place
 */
+
 export const bubbleSort = (array) => {
-    const sortedArray = [];
-    return sortedArray;
-} 
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        }
+    }
+    return array;
+};
 
-
+// const swap = (arr, indexOne, indexTwo) => {
+//     const temp = arr[indexTwo];
+//     arr[indexTwo] = arr[indexOne];
+//     arr[indexOne] = temp;
+//   }
--- src/countingSort.mjs
+++ src/countingSort.mjs
@@ -1,11 +1,32 @@
 /*
-Conceptual description:
-Time Complexity:
+Conceptual description: non-comparison sorting algo, stable
+Time Complexity: O(n + k) n = size of input list, k = max value in list
 Space Complexity:
 */
 export const countingSort = (array) => {
-    const sortedArray = [];
-    return sortedArray;
+    const max = Math.max(...array);
+    const auxArray = new Array(max + 1);
+    
+    for (let i = 0; i < auxArray.length; i++) {
+        auxArray[i] = 0;
+    }
+
+    for (let i = 0; i < auxArray.length; i++) {
+        auxArray[array[i-1]] += 1;
+    }
+
+    let index = 0;
+
+    for (let i = 0; i < auxArray.length; i++) {
+        if (auxArray[i] > 0) {
+            for (let j = 0; j < auxArray[i]; j++ ) {
+                array[index] = i
+                index++
+            }
+        }
+    }
+
+    return array;
 } 
 
 
